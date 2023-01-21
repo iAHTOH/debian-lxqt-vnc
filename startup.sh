129274f690348004970614fff6f74de7c0dd3599
iAHTOH <k.ahtoh@gmail.com>
2023-01-21 14:12:38 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 set SCREEN_RESOLUTION 1920x1080
 catch {set SCREEN_RESOLUTION $env(resolution)}
 
-spawn /usr/bin/vncserver :1 -fg -geometry $SCREEN_RESOLUTION
+/usr/bin/vncserver :1 -fg -geometry $SCREEN_RESOLUTION
 
 set timeout -1
 expect eof
